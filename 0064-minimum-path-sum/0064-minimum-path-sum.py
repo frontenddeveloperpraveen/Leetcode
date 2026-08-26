be0e835783c6dfe3d1f2976d260d841d443f029d
Praveen KR <124413604+frontenddeveloperpraveen@users.noreply.github.com>
2026-08-26 20:43:03 +0530

    Time: 46 ms (8.54%), Space: 32.5 MB (6.35%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        # Possible action right and down
+        memo = {}
+        def dfs(m,n):
+            if((m,n) in memo):
+                return memo[(m,n)]
+            if(m==0 and n==0):
+                memo[(m,n)] = grid[m][n]
+                return memo[(m,n)]
+            
+            if(m < 0 or n <0):
+                memo[(m,n)] = float('inf')
+                return float('inf')
+
+            # left movie 
+            left = dfs(m,n-1)
+
+            #Down
+            up = dfs(m-1,n)
+
+            memo[(m,n)] = min(left,up) + grid[m][n]
+            return memo[(m,n)]
+
+        return dfs(len(grid)-1,len(grid[0])-1)
+
+
+        
